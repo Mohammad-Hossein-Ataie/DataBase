3f55a7923be2d5bfc4e282a599653066ba01ea89
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2021-10-26 23:54:10 +0330

    Order handling

--- Q9.sql
+++ Q9.sql
@@ -27,3 +27,38 @@ create table activates_on(
   PRIMARY KEY(paikname),
   FOREIGN KEY(paikname) REFERENCES Paik(paikname) ON DELETE CASCADE
 );
+CREATE TABLE Orders(
+  rname char(20),
+  pname char(20),
+  date CHAR(20),
+  state CHAR(20),
+  primary key(rname, pname),
+  foreign key(rname, pname) references Food_serve(rname, fname) on delete cascade
+);
+create table sends(
+  rname char(20),
+  pname char(20),
+  paikname char(20),
+  primary key(rname, pname, paikname),
+  foreign key(paikname) references paik(paikname),
+  foreign key(rname, pname) references Orders(rname, fname)
+);
+CREATE TABLE Normal_order(
+  rname char(20),
+  pname char(20),
+  primary key(rname, pname),
+  foreign key(rname, pname) references Orders(rname, pname) on delete cascade
+);
+create table Discount(
+  dcode integer,
+  exp_date char(20),
+  primary key(dcode)
+);
+create table Special_order(
+  rname char(20),
+  pname char(20),
+  dcode integer,
+  primary key(rname, pname, dcode),
+  foreign key(rname, pname) references Orders(rname, pname) on delete cascade,
+  foreign key(dcode) references Discount(dcode) on delete no action
+);
